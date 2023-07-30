41f402d81b8e739e1ee7f72b0567474eec0b995b
Dario Russo <dariorusso987@gmail.com>
2023-07-30 13:17:56 +0200

    first commit

---
+++ src/component/Expenses/Expense.js
@@ -0,0 +1,32 @@
+import ExpensiveItem from "./ExpensiveItem";
+import "./Expense.css";
+import Card from '../UI/Card';
+    
+function Expense(props) {
+  return (
+    <Card className="expenses">
+      <ExpensiveItem
+        title={props.items[0].title}
+        amount={props.items[0].amount}
+        date={props.items[0].date}
+      />
+      <ExpensiveItem
+        title={props.items[1].title}
+        amount={props.items[1].amount}
+        date={props.items[1].date}
+      />
+      <ExpensiveItem
+        title={props.items[2].title}
+        amount={props.items[2].amount}
+        date={props.items[2].date}
+      />
+      <ExpensiveItem
+        title={props.items[3].title}
+        amount={props.items[3].amount}
+        date={props.items[3].date}
+      />
+    </Card>
+  );
+}
+
+export default Expense;
